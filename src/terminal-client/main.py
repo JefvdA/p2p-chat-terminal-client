c31e7bb598bd2acd6040f65ebde3873bbbee0db5
JefvdA <jef.v.d.a@live.be>
2023-02-19 22:21:25 +0100

    feat: added typer setup

---
+++ src/terminal-client/main.py
@@ -0,0 +1,17 @@
+import typer
+
+app = typer.Typer()
+
+
+@app.command()
+def hello():
+    print("Hello world!")
+
+
+@app.command()
+def goodbye():
+    print("Goodbye world!")
+
+
+if __name__ == '__main__':
+    app()
